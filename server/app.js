5f79d30c883dfde168f8f8b826d1fa23755e1072
SRI <srihari2426@gmail.com>
2025-05-28 18:57:20 +0530

    Database connection

--- server/app.js
+++ server/app.js
@@ -1,10 +1,26 @@
 require('dotenv').config({ path : '../.env' });
 const express = require('express');
+const db = require('./db');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
+
+async function start() {
+    try{
+        await db.query('SELECT 1');
+        console.log('Database connection successful');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (err){
+        console.error('Database connection failed:', err);
+        process.exit(1); // Exit the process with failure
+    }
+}
+
+start();
